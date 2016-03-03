1a36311b8aa05f9e5b8d969fb6f8ddc932844c0b
Sean Milburn <sean.milburn@digital.hmrc.gov.uk>
2016-03-03 09:47:41 +0000

    Added comments to code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
 
 
 angular.module('seanMilburnEssensysApp', ['ngRoute'])  
-    
+    //Template config set up for scalability
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {
@@ -13,9 +13,9 @@ angular.module('seanMilburnEssensysApp', ['ngRoute'])
                 redirectTo: '/'
             });
     })
-    
+    //Factory service to import the csv file
     .factory('ParserService', function($http, $q) {
-
+        //url to dropbox, where the csv is stored on the cloud
         var csvUrl = "https://dl.dropboxusercontent.com/s/gqe0q7j0fyc5ov2/Test_csv.csv";
 
         var statusCode;
@@ -24,14 +24,18 @@ angular.module('seanMilburnEssensysApp', ['ngRoute'])
             $http({
                 url: csvUrl
             }).
+            //Success promise return data for headers and rows
             success(function(response, status) {
                 statusCode = status;
+                //Pushes csv data to array
                 var data = $.csv.toArrays(response);
                 deferred.resolve({
+                    //formats csv data
                     'header': data[0],
                     'rows': data.slice(1, data.length)
                 });
             }).
+            //Returns error response if promise is not met
             error(function(response, status) {
                 statusCode = status;
             });
@@ -41,11 +45,12 @@ angular.module('seanMilburnEssensysApp', ['ngRoute'])
             getData: getData
         };
     })
-    
+    //Controller to populate view with data
     .controller('myCtrl', function($scope, ParserService) {
         
         $scope.header = [];
         $scope.rows = [];
+        //getData function to populate table
         $scope.getData = function() {
             ParserService.getData().then(function(result) {
                 $scope.header = result.header;
@@ -54,10 +59,12 @@ angular.module('seanMilburnEssensysApp', ['ngRoute'])
                 })
             })
         };
+        //Function toggle button label for accordian functionality
         $scope.toggleLabel = function() {
             $('.btn').text(function(i, text){
                 return text === "Show Table" ? "Hide Table" : "Show Table";
             })
         };
+        //Call getData function on initialize
         $scope.getData();
     });
